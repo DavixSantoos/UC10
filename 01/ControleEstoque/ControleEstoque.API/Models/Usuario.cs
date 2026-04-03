377429135af9bd4f213fdb19e41ffea73c8773d4
AECIO MENDES BRITO <aeciobrito@gmail.com>
2026-04-02 22:11:54 -0300

    Criacao das models de Usuario

---
+++ 01/ControleEstoque/ControleEstoque.API/Models/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.API.Models
+{
+    public enum PerfilUsuario { Cliente, Caixa, Gerente }
+
+    public abstract class Usuario // abstract, impede instancia da classe
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required, EmailAddress, StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public string SenhaHash { get; set; } // aplicar tamanho mínimo
+
+        [Required]
+        public PerfilUsuario Perfil { get; set; }
+    }
+
+    public class Cliente : Usuario
+    {
+        [StringLength(14)]
+        public string CPF { get; set; }
+    }
+
+    public class Caixa : Usuario
+    {
+        [StringLength(50)]
+        public string Turno { get; set; }
+    }
+
+    public class Gerente : Usuario
+    {
+        [StringLength(50)]
+        public string Setor { get; set; }
+    }
+}
