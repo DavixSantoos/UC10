09c13011a33e721d5411c246ef3d7ff828e449fc
David C Santos <133790186+DavixSantoos@users.noreply.github.com>
2026-04-13 22:04:40 -0300

    Controle

---
+++ 01/ControleEstoque/ControleEstoque.API/Controllers/FornecedoresController.cs
@@ -0,0 +1,41 @@
+﻿using ControleEstoque.API.DTOs;
+using ControleEstoque.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleEstoque.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FornecedoresController : ControllerBase
+    {
+        private readonly IFornecedorService _FornecedorService;
+
+        public FornecedoresController(IFornecedorService fornecedorService)
+        {
+            _FornecedorService = fornecedorService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        {
+           var fornecedores = await _FornecedorService.ObterTodosAsync();
+            return Ok(fornecedores);
+        }
+        [HttpGet("{id}")]//recebe  rota 
+        public async Task<IActionResult> GetById(int id) 
+        {
+           var result = await _FornecedorService.ObterPorIdAsync(id);
+            if (result == null) NotFound();
+      
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]CriarFornecedorDto dto)
+        { 
+          FornecedorDto result = await _FornecedorService.CriarAsync(dto);
+           
+            return Created(nameof(Create), result);
+        }
+    }
+  
+}
--- 01/ControleEstoque/ControleEstoque.API/Models/Fornecedor.cs
+++ 01/ControleEstoque/ControleEstoque.API/Models/Fornecedor.cs
@@ -1,7 +1,18 @@
-﻿namespace ControleEstoque.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.API.Models
 {
     public class Fornecedor
     {
+        [Key]
+        public int Id { get; set; }
 
+        [Required, StringLength(100)]
+        public string NomeFantasia { get; set; }
+
+        [Required, StringLength(14)]
+        public string CNPJ { get; set; }
+
+        public ICollection<Produto> Produtos { get; set; }
     }
 }
